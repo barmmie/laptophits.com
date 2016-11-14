b4c95eec46fa99c54eaffec49f78a89e43b3b6e9
Marcin Doliwa <mdoliwa@gmail.com>
2016-11-14 19:16:36 +0100

    add url to Comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161114173215) do
+ActiveRecord::Schema.define(version: 20161114180249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20161114173215) do
     t.string   "subreddit_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "url"
   end
 
   create_table "mentions", force: :cascade do |t|
