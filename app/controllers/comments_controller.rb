596cc82daff27b70f4dfdce4102e38d47bd2fe6c
Marcin Doliwa <mdoliwa@gmail.com>
2016-11-16 15:33:33 +0100

    add after params to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,5 +2,6 @@ class CommentsController < ApplicationController
   def index
     @product = Product.find(params[:product_id])
     @comments = @product.comments.order('comments.created_utc DESC')
+    @comments = @comments.where('comments.created_utc > ?', Time.now - ::TimeAbbr.to_time(params[:after])) if params[:after]
   end
 end
