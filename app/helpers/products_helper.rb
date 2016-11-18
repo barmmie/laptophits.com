a7c7cafbbb84e336ecbb356770f2124d6652ed17
Marcin Doliwa <mdoliwa@gmail.com>
2016-11-18 17:14:18 +0100

    update index products design

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,8 +1,2 @@
 module ProductsHelper
-  def product_row product
-    product_price = "$#{product.price_in_cents/100.to_f}" if product.price_in_cents
-    [link_to(product.title, product.offer_url),
-     product_price,
-     link_to("#{product.product_count} mentions", product_comments_path(product, after: params[:after]))].reject(&:nil?).join(' - ').html_safe
-  end
 end
