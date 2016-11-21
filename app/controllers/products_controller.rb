5b1a3baeeade4caa6b1a57b69d3cad46f3ccda30
Marcin Doliwa <mdoliwa@gmail.com>
2016-11-21 16:31:17 +0100

    add brand filters to sidebar

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
     @mentions = Mention.order('created_at desc')
 
     @products = Product.joins(:comments).select('products.*, count(mentions.product_id) as "product_count"').group('products.id').order('product_count DESC')
+    @brands = @products.map(&:brand).uniq.map{|brand| [brand,0]}.to_h
 
     if params[:after]
       time_after = Time.now - ::TimeAbbr.to_time(params[:after])
@@ -15,6 +16,12 @@ class ProductsController < ApplicationController
 
     if params[:price_to]
       @products = @products.where('price_in_cents < ?', params[:price_to].to_f*100)
+    end
+
+    @brands = @brands.merge(@products.map(&:brand).group_by(&:itself).map{|k,v| [k,v.length]}.to_h).sort.to_h
+    
+    if params[:brand]
+      @products = @products.where('brand = ?', params[:brand])
     end
 
     @products = @products.paginate(page: params[:page], per_page: 10)
