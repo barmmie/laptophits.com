04410bf8fe149205991d8f914eb43a962b848ea2
Marcin Doliwa <mdoliwa@gmail.com>
2016-11-25 16:51:35 +0100

    filter refactoring

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,30 +1,16 @@
 class ProductsController < ApplicationController
   def index
-    @mentions = Mention.order('created_at desc')
+    @mentions_count = Mention.count
 
-    @products = Product.joins(:comments).select('products.*, count(mentions.product_id) as "product_count"').group('products.id').order('product_count DESC')
+    @products = Product.filter_by(filter_params).paginate(page: params[:page], per_page: 10)
+
     @brands = @products.map(&:brand).uniq.map{|brand| [brand,0]}.to_h
 
-    if params[:after]
-      time_after = Time.now - ::TimeAbbr.to_time(params[:after])
-      @products = @products.where('comments.created_utc > ?',time_after)
-    end
+  end
 
-    if params[:price_from]
-      @products = @products.where('price_in_cents >= ?', params[:price_from].to_f*100)
-    end
+  private 
 
-    if params[:price_to]
-      @products = @products.where('price_in_cents < ?', params[:price_to].to_f*100)
-    end
-
-    @brands = @brands.merge(@products.map(&:brand).group_by(&:itself).map{|k,v| [k,v.length]}.to_h).sort.to_h
-    @brands['Other'] = @brands.delete 'Other' if @brands['Other']
-    
-    if params[:brand]
-      @products = @products.where('brand = ?', params[:brand])
-    end
-
-    @products = @products.paginate(page: params[:page], per_page: 10)
+  def filter_params
+    params.slice(:after, :price_from, :price_to, :brand)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,35 @@
 class Product < ActiveRecord::Base
   has_many :mentions
   has_many :comments, through: :mentions
+
+  def self.filter_by(filter_params)
+    results = joins(:comments).
+      select('products.*, count(mentions.product_id) as "mentions_count"').
+      group('products.id').
+      order('mentions_count DESC')
+
+    filter_params.each do |key, value|
+      results = results.public_send(key, value) if value.present?
+    end
+
+    results
+  end
+
+  def self.after(time)
+      where('comments.created_utc > ?', Time.now - ::TimeAbbr.to_time(time))
+  end
+
+  def self.price_from(price)
+    where('price_in_cents >= ?', price.to_f*100)
+  end
+
+  def self.price_to(price)
+    where('price_in_cents < ?', price.to_f*100)
+  end
+
+  def self.brand(brand_name)
+    where(brand: brand_name)
+  end
+
+
 end
