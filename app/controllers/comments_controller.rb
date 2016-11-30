37ad35de10570d50dedd8aa1f58da0296349c6d3
Marcin Doliwa <mdoliwa@gmail.com>
2016-11-30 10:38:23 +0100

    add comments pagination

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,5 +3,6 @@ class CommentsController < ApplicationController
     @product = Product.find(params[:product_id])
     @comments = @product.comments.order('comments.created_utc DESC')
     @comments = @comments.where('comments.created_utc > ?', Time.now - ::TimeAbbr.to_time(params[:after])) if params[:after]
+    @comments = @comments.paginate(page: params[:page], per_page: 25)
   end
 end
