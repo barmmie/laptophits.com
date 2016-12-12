84605e9b53e0e98df7f275ea7e3a2356e6148a97
Marcin Doliwa <mdoliwa@gmail.com>
2016-12-12 17:12:33 +0100

    add display resolution extracting

--- app/models/specification.rb
+++ app/models/specification.rb
@@ -52,4 +52,16 @@ class Specification
     match = text.match(re)
     match.captures.reject(&:nil?).first if match.present?
   end
+
+  def extract_display_resolution
+    text = data_sources[:amazon_api_data]['features'].unshift(data_sources[:amazon_api_data]['title']).join(',')
+
+    re_array = [
+      /(\d{3,} ?x ?\d{3,})/i
+    ]
+
+    re = Regexp.union(re_array)
+    match = text.match(re)
+    match.captures.reject(&:nil?).first.downcase.gsub(/\s+/,"") if match.present?
+  end
 end
