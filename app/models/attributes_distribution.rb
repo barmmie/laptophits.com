518567cd28aa223aa3fa4710e6690168a7ed2379
Marcin Doliwa <mdoliwa@gmail.com>
2016-12-19 12:06:18 +0100

    fix merge conflicts

--- app/models/attributes_distribution.rb
+++ app/models/attributes_distribution.rb
@@ -1,9 +1,5 @@
 class AttributesDistribution
-<<<<<<< HEAD
   ATTRIBUTES = Specification::SPEC_PARAMS.merge(price: :number)
-=======
-  ATTRIBUTES = Specification::SPEC_PARAMS.merge(price: :number])
->>>>>>> refactoring
   ATTR_PARAMS = {brand: [:brand],
                  price: [:price_from, :price_to],
                  display_size: [:display_size_from, :display_size_to],
