e524a5bf34c358d10a6634be0468b8cc68b805cf
Marcin Doliwa <mdoliwa@gmail.com>
2017-01-05 17:42:10 +0100

    refactor specification filters display

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,76 +1,20 @@
 module ProductsHelper
-  def brand_filter(brands, params)
-    list = brands.map do |brand, count|
+  def filter(attribute_name, distribution, params)
+    list = distribution.map do |value, count|
       "<li>" + 
-        link_to_unless(params[:brand] == brand,
-        "#{brand} (#{count})",
-        products_path(params.except(:controller, :action, :page).merge({brand: brand}))) +
+        link_to_unless(params[attribute_name] == value,
+        "#{value} (#{count})",
+        products_path(params.except(:controller, :action, :page).merge({attribute_name => value}))) +
       "</li>"
     end.join
     
     list = "<li>" + 
-      link_to_unless(params[:brand].nil?,
-      "All brands",
-      products_path(params.except(:controller, :action, :page).merge(brand: nil))) + 
+      link_to_unless(params[attribute_name].nil?,
+      "All #{attribute_name.to_s.pluralize}",
+      products_path(params.except(:controller, :action, :page).merge(attribute_name => nil))) + 
       "</li>" + list
 
     "<ul>#{list}</ul>".html_safe
-  end
-
-  def operating_system_filter(operating_systems, params)
-    list = operating_systems.map do |operating_system, count|
-      "<li>" + 
-        link_to_unless(params[:operating_system] == operating_system,
-        "#{operating_system} (#{count})",
-        products_path(params.except(:controller, :action, :page).merge({operating_system: operating_system}))) +
-      "</li>"
-    end.join
-    
-    list = "<li>" + 
-      link_to_unless(params[:operating_system].nil?,
-      "All OS",
-      products_path(params.except(:controller, :action, :page).merge(operating_system: nil))) + 
-      "</li>" + list
-
-    "<ul>#{list}</ul>".html_safe
-
-  end
-
-  def hdd_type_filter(hdd_types, params)
-    list = hdd_types.map do |hdd_type, count|
-      "<li>" + 
-        link_to_unless(params[:hdd_type] == hdd_type,
-        "#{hdd_type} (#{count})",
-        products_path(params.except(:controller, :action, :page).merge({hdd_type: hdd_type}))) +
-      "</li>"
-    end.join
-    
-    list = "<li>" + 
-      link_to_unless(params[:hdd_type].nil?,
-      "All HDD types",
-      products_path(params.except(:controller, :action, :page).merge(hdd_type: nil))) + 
-      "</li>" + list
-
-    "<ul>#{list}</ul>".html_safe
-  end
-
-  def processor_filter(processors, params)
-    list = processors.map do |processor, count|
-      "<li>" + 
-        link_to_unless(params[:processor] == processor,
-        "#{processor} (#{count})",
-        products_path(params.except(:controller, :action, :page).merge({processor: processor}))) +
-      "</li>"
-    end.join
-    
-    list = "<li>" + 
-      link_to_unless(params[:processor].nil?,
-      "All Processor Types",
-      products_path(params.except(:controller, :action, :page).merge(processor: nil))) + 
-      "</li>" + list
-
-    "<ul>#{list}</ul>".html_safe
-
   end
 
   def display_size_filter(display_sizes, params)
@@ -107,26 +51,6 @@ module ProductsHelper
         params[:ram_size].nil?,
         "All RAM sizes",
         products_path(params.except(:controller, :action, :page).merge(ram_size: nil))) +
-      "</li>" + list
-
-    "<ul>#{list}</ul>".html_safe
-  end
-
-  def display_resolution_filter(display_resolutions, params)
-    list = display_resolutions.map do |display_resolution, count|
-      "<li>" + 
-        link_to_unless(
-          params[:display_resolution] == display_resolution,
-          "#{display_resolution} (#{count})",
-          products_path(params.except(:controller, :action, :page).merge(display_resolution: display_resolution))) +
-      "</li>"
-    end.join
-
-    list = "<li>" + 
-      link_to_unless(
-        params[:display_resolution].nil?,
-        "All resolutions",
-        products_path(params.except(:controller, :action, :page).merge(display_resolution: nil))) +
       "</li>" + list
 
     "<ul>#{list}</ul>".html_safe
